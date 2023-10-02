362413b38bc5f087005a28a7c93bd453bd83414f
Сергій <ikot@MacBook-Pro-Sergij.local>
2023-10-02 21:18:00 +0300

    lesson1

---
+++ lesson1/code/index.js
@@ -0,0 +1,66 @@
+// var string = 'Hello world'
+// let string2 = 'Text'
+// const string3 = "Text"
+// const string3 = `Text`
+// const number = 4
+
+// alert('');
+
+// [1, 2].forEach(alert);
+
+// let num1 = 3;
+// let num2 = 4;
+
+// let sum = num1 + num2;
+
+// alert(++sum)
+// alert(--sum)
+
+// let a;
+// let sum_of_numbers;
+// let SumOfNumbers;
+// let sumOfNumbers;
+
+// snakeCase - sum_of_numbers
+// camelCase - sumOfNumbers
+// kebabCase - sum-of-numbers
+// pascalCase - SumOfNumbers
+
+// const user = {}
+// const userName = 'Name'
+// const age = 30
+
+// var number = 5;
+// number = 6
+
+// let number2 = 5;
+// number2 = 7;
+
+// const number4 = 5;
+// number4 = 7;
+
+// alert(number2);
+
+// let string = 'Hello';
+// let string2 = 'world';
+// let string3 = '!!!';
+
+// let union = string + ' ' + string2 + string3;
+
+// // alert(union);
+
+// let message = prompt('Enter your age', 0);
+// let message2 = confirm('Are you ok?');
+// console.log(message2);
+
+/*
+*
+* Hello comment
+* string
+*
+*/
+
+
+// Hello comment
+
+
---
+++ lesson1/homework/homework.md
@@ -0,0 +1,12 @@
+# Task 1
+
+## Hello user
+
+Використовуючи alert та prompt створити пешу програму за таким функціоналом:
+
+Привітатись з юзером  - "Hello!";
+Спитати імя - "What is your name?";
+Спитати з якої він країни - "Where are you from?";
+Спитати рік нродження - "What year were you born"?;
+Порахувати скільки років юзеру;
+Вивести на екран -  "Nice to meet you, {name}! You are {age}! You are from {country}!"
