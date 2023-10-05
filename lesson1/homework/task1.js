c78950680dfcac4a9bdc875227742c9fd9dc6bce
iKot <sergey.savchuk91@gmail.com>
2023-10-05 21:55:25 +0300

    added second lesson

---
+++ lesson1/homework/task1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let userName;
+let userCountry;
+
+alert('Hello!');
+userName = prompt('What is your name?');
+userCountry = prompt('Where are you from?');
+alert('Nice to meet you, ' + userName + '! ' + 'You are from ' + userCountry + '!');
