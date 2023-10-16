dae357f0aa9e9d71ceb8a52293b26457f1c3d71c
iKot <sergey.savchuk91@gmail.com>
2023-10-16 21:30:10 +0300

    added lesson5

---
+++ lesson5/assets/Links.md
@@ -0,0 +1,49 @@
+# Документації
+
+## String
+
+[String](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String)
+
+[Корисні методи строк](https://developer.mozilla.org/ru/docs/Learn/JavaScript/First_steps/Useful_string_methods)
+
+[UTF-8](https://ru.wikipedia.org/wiki/UTF-8)
+
+[UTF-16](https://ru.wikipedia.org/wiki/UTF-16)
+
+[Специфікація](https://tc39.es/ecma262/multipage/text-processing.html#sec-string-objects)
+
+## Nimber
+
+[Number](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number)
+
+[Специфікація](https://tc39.es/ecma262/#sec-number-objects)
+
+## BigInt
+
+[BigInt](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/BigInt)
+
+[Специфікація](https://tc39.es/ecma262/#sec-bigint-objects)
+
+## Boolean
+
+[Boolean](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Boolean)
+
+[Специфікація](https://tc39.es/ecma262/#sec-boolean-objects)
+
+## null
+
+[null](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/null)
+
+[Специфікація](https://tc39.es/ecma262/multipage/overview.html#sec-null-value)
+
+## undefined
+
+[undefined](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/undefined)
+
+[Специфікація](https://tc39.es/ecma262/multipage/global-object.html#sec-undefined)
+
+## Symbol
+
+[Symbol](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Symbol)
+
+[Специфікація](https://tc39.es/ecma262/multipage/fundamental-objects.html#sec-symbol-objects)
---
+++ lesson5/code/index.js
@@ -0,0 +1,68 @@
+// const string = 'Good';
+
+// console.log(string.length);
+// console.log(string[0]);
+// console.log(string.at(0));
+// console.log(string[string.length - 1]);
+// console.log(string.at(-1));
+
+// string[0] = 'D'
+// console.log(string)
+
+// console.log(string.toUpperCase())
+// console.log(string.toLowerCase())
+// const newChar = string[1].toUpperCase();
+// console.log(newChar)
+
+// const string2 = 'good by, good day';
+// console.log(string2.indexOf('good'));
+// console.log(string2.indexOf('good', 2));
+// console.log(string2.lastIndexOf('good', 5));
+//
+// /*
+// ~n = -(n + 1)
+// ~0 = -1
+//  */
+//
+// if (~string2.indexOf('good')) {
+//     console.log('in if')
+// }
+
+// console.log(string2.includes('good'))
+// console.log(string2.includes(string.toLowerCase()))
+
+// console.log(string2.startsWith('good'));
+// console.log(string2.slice(1, 5));
+// console.log(typeof string2.slice(5, 1));
+// console.log(string2.substring(1, 5));
+// console.log(string2.substring(5, 1));
+// console.log(string2.replace('go', 'Go'));
+// console.log(string2.replace(RegExp('Go', 'ig'), 'Go'));
+// console.log(string2.replace(/good(?!.*good)/, 'Good'));
+
+// const string3 = '                trim                   ';
+// console.log(string3.trim())
+
+// console.log(string2.split())
+// console.log(string2.split(''))
+// console.log(string2.split(', '))
+
+// Number
+
+// console.log(isNaN('NaN')) // true
+// console.log(Number.isNaN('NaN')) // false
+// console.log(isNaN(undefined)) // true
+// console.log(Number.isNaN(undefined)) // false
+// console.log(isNaN({})) // true
+// console.log(Number.isNaN({})) // false
+// console.log(isNaN('text')) // true
+// console.log(Number.isNaN('text')) // false
+
+// Boolean
+//
+// console.log(new Boolean());
+// console.log(new Boolean(false));
+// console.log(new Boolean(undefined));
+// console.log(new Boolean(true));
+// console.log(Boolean(NaN);
+
